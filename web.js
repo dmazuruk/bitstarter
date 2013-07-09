2f21a472d1c170ff78a9528df308ec936aaaa432
Dawid Mazuruk <dmazuruk@gmail.com>
2013-07-09 16:57:07 -0400

    Changed web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFileSync('/index.html', function(err, data) {
+    fs.readFileSync('index.html', function(err, data) {
 	if (err) throw err;
 	var buffer = new Buffer(16);
 	buffer.write(data, 'utf-8');    
